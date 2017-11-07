ce9cfa550439af8e86ca8cce335af9aff729f2d8
Alyona Anikeieva <alyonaoooooooo@gmail.com>
2017-11-07 02:04:00 +0200

    code was fixed (I was add a message of errror and  new condition about it)

--- 3_lesson/js/some_func.js
+++ 3_lesson/js/some_func.js
@@ -1,76 +1,93 @@
 // # TEMPLATE
 
-var main_content = document.querySelector('.main-content');
-var json_url = decodeURIComponent(location.search.slice(6));
+(function some_func() {
+	var main_content = document.querySelector('.main-content');
+	var error = document.querySelector('.hidden');
+	var json_url = decodeURIComponent(location.search.slice(6));
 
-var json_validation = (url) => {
-	var pattern = /https?:\/\/w{3}.json-generator\.com\/api\/json\/get\/\w+\?indent=\d/g;
-	if(pattern.test(url)) return url;
-	// console.log(url);
-};
+	function json_validation(url) {
+		var pattern = /(https?:\/\/)?w{3}.json-generator\.com\/api\/json\/get\/\w+\?indent=\d/g;
+		if(pattern.test(url)) {
+			MyApp(url);
+		}
+		else {
+			error.classList.remove('hidden');
+			error.classList.add('open');
+			console.log('open');
+		}
+	};
+	json_validation(json_url);
 
-( function MyApp(){
-  // console.log('init function');
-  // https://www.json-generator.com/#
-  var fetchedData = fetch(json_validation(json_url)).then(function(response) {
-    return response.json();
-  }).then(data => {
-    renderInterface(data);
-  });
-}());
+	function MyApp(val){
+		// https://www.json-generator.com/#
+		var fetchedData = fetch(val).then(function(response) {
+		return response.json();
+		}).then(data => {
+		renderInterface(data);
+		});
+	};
 
-function renderInterface( data ) {
-	var array_1 = [],
-		array_2 = [],
-		array_3 = [],
-		prop,
-		copy_data;
+	function renderInterface( data ) {
+		var array_1 = [],
+			array_2 = [],
+			array_3 = [],
+			fruit,
+			balances,
+			eye_female_isActive,
+			prop,
+			copy_data;
 
-	copy_data = data;
-	// console.log(copy_data);
+		copy_data = data;
 
-	for (var i = 0; i < copy_data.length; i++) {
-		copy_data[i].balance = copy_data[i].balance.slice(1).replace(/\,/g, '');
+		for (var i = 0; i < copy_data.length; i++) {
+			copy_data[i].balance = copy_data[i].balance.slice(1).replace(/\,/g, '');
+		}
+
+		fruit = copy_data.filter((item, i, arr) => {
+			return arr[i].favoriteFruit === 'banana';
+		});
+		for (var j = 0; j < fruit.length; j++) {
+			array_1.push(fruit[j].name);
+		}
+		// console.log(array_1);
+		result(array_1, 'Peoples, those like bananas', 'like_bananas');
+
+		balances = copy_data.filter((item, i, arr) => {
+			return arr[i].balance > 2000 && arr[i].age > 25;
+		});
+		for (var j = 0; j < balances.length; j++) {
+			array_2.push(balances[j].name);
+		}
+		// console.log(array_2);
+		result(array_2, 'Peoples, those are more than 25 and have on balance more than $2000', 'more_than_2000');
+
+		eye_female_isActive = copy_data.filter((item, i, arr) => {
+			return arr[i].eyeColor === 'blue' && arr[i].gender === 'female' && arr[i].isActive === false;
+		});
+		for (var j = 0; j < eye_female_isActive.length; j++) {
+			array_3.push(eye_female_isActive[j].name);
+		}
+		// console.log(array_3);
+		result(array_3, 'Do not active Peoples, those have blue eyes, female gender', 
+			'blue_eyes');
 	}
 
-	var fruit = copy_data.filter((item, i, arr) => {
-		return arr[i].favoriteFruit === 'banana';
-	});
-	for (var j = 0; j < fruit.length; j++) {
-		array_1.push(fruit[j].name);
-	}
-	console.log(array_1);
-	result(array_1, 'Peoples, those like bananas', 'like_bananas');
+	var result = (arr, h2, id) => {
+		main_content.insertAdjacentHTML('beforeEnd', '<div class="list_one_f_array"><h2>'+ h2 + 
+			'</h2><ul id="'+ id +'"></ul></div>');
+		var ul = document.querySelector('#' + id);
 
-	var balances = copy_data.filter((item, i, arr) => {
-		return arr[i].balance > 2000 && arr[i].age > 25;
-	});
-	for (var j = 0; j < balances.length; j++) {
-		array_2.push(balances[j].name);
+		if(arr.length > 0) {
+			arr.forEach((item, i, array) => {
+				ul.insertAdjacentHTML('beforeEnd', '<li>' + item + '</li>');
+			});
+		}
+		else {
+			ul.insertAdjacentHTML('beforeEnd', '<li class="none">none</li>');	
+		}
 	}
-	console.log(array_2);
-	result(array_2, 'Peoples, those are more than 25 and have on balance more than $2000', 'more_than_2000');
 
-	var eye_female_isActive = copy_data.filter((item, i, arr) => {
-		return arr[i].eyeColor === 'blue' && arr[i].gender === 'female' && arr[i].isActive === false;
-	});
-	for (var j = 0; j < eye_female_isActive.length; j++) {
-		array_3.push(eye_female_isActive[j].name);
-	}
-	console.log(array_3);
-	result(array_3, 'Do not active Peoples, those have blue eyes, female gender', 
-		'blue_eyes');
-}
-
-function result(arr, h2, id) {
-	main_content.insertAdjacentHTML('beforeEnd', '<div class="list_one_f_array"><h2>'+ h2 + 
-		'</h2><ul id="'+ id +'"></ul></div>');
-	var ul = document.querySelector('#' + id);
-	// console.log(ul);
-	arr.forEach((item, i, array) => {
-		ul.insertAdjacentHTML('beforeEnd', '<li>' + item + '</li>');
-	});
-} 
+})();
 
 // Задача:
 // Написать ф-ю которая принимает на вход обьект с сервера и
