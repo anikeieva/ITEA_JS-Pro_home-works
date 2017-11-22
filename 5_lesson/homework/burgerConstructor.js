1c8edeb0c895ac80db3efab7e8e2ec3d4cb9fab6
Alyona Anikeieva <alyonaoooooooo@gmail.com>
2017-11-23 00:02:52 +0200

    Optimizated code, updated styles, bugfix

--- 5_lesson/homework/burgerConstructor.js
+++ 5_lesson/homework/burgerConstructor.js
@@ -1,14 +1,9 @@
 
-
   /*
-
     Задание:
-
       1. Создать конструктор бургеров на прототипах, которая добавляет наш бургер в массив меню
-
       Дожно выглядеть так:
       new burger('Hamburger',[ ...Массив с ингредиентами ] , 20);
-
       Прототип для бургера:
       {
         cookingTime: 0,     // Время на готовку
@@ -23,12 +18,9 @@
           }
         }
       }
-
       Результатом конструктора нужно вывести массив меню c добавленными элементами.
       // menu: [ {name: "", composition: [], cookingTime:""},  {name: "", composition: [], cookingTime:""}]
-
         2. Создать конструктор заказов
-
         Прототип:
         {
           id: "",
@@ -37,7 +29,6 @@
           orderException: "",
           orderAvailability: ""
         }
-
           Заказ может быть 3х типов:
             1. В котором указано название бургера
               new Order('Hamburger'); -> Order 1. Бургер Hamburger, будет готов через 10 минут.
@@ -45,264 +36,330 @@
               new Order('', 'has', 'Название ингредиента') -> Order 2. Бургер Чизбургер, с сыром, будет готов через 5 минут.
             3. В котором указано чего не должно быть
               new Order('', 'except', 'Название ингредиента') ->
-
-
             Каждый их которых должен вернуть статус:
             "Заказ 1: Бургер ${Название}, будет готов через ${Время}
-
             Если бургера по условиям заказа не найдено предлагать случайный бургер из меню:
               new Order('', 'except', 'Булка') -> К сожалению, такого бургера у нас нет, можете попробовать "Чизбургер"
               Можно например спрашивать через Confirm подходит ли такой вариант, если да - оформлять заказ
               Если нет, предложить еще вариант из меню.
-
         3. Протестировать программу.
           1. Вначале добавляем наши бургеры в меню (3-4 шт);
           2. Проверяем работу прототипного наследования вызывая метод showComposition на обьект бургера с меню
           3. Формируем 3 заказа
-
         Бонусные задания:
         4. Добавлять в исключения\пожелания можно несколько ингредиентов
         5. MEGABONUS
           Сделать графическую оболочку для программы.
-
   */
 
 
 // код незавершений!!! ще графіка і оптимізація
-  const Ingredients = [
-    'Булка',
-    'Огурчик',
-    'Котлетка',
-    'Бекон',
-    'Рыбная котлета',
-    'Соус карри',
-    'Кисло-сладкий соус',
-    'Помидорка',
-    'Маслины',
-    'Острый перец',
-    'Капуста',
-    'Кунжут',
-    'Сыр Чеддер',
-    'Сыр Виолла',
-    'Сыр Гауда',
-    'Майонез'
-  ];
+const Ingredients = [
+  'Булка',
+  'Огурчик',
+  'Котлетка',
+  'Бекон',
+  'Рыбная котлета',
+  'Соус карри',
+  'Кисло-сладкий соус',
+  'Помидорка',
+  'Маслины',
+  'Острый перец',
+  'Капуста',
+  'Кунжут',
+  'Сыр Чеддер',
+  'Сыр Виолла',
+  'Сыр Гауда',
+  'Майонез'
+];
 
-  const cheesburgerIngredients = [
-    'Булка',
-    'Огурчик',
-    'Котлетка',
-    'Кисло-сладкий соус',
-    'Помидорка',
-    'Салат',
-    'Кунжут',
-    'Сыр Чеддер',
-    'Майонез'
-  ];
+const cheesburgerIngredients = [
+  'Булка',
+  'Огурчик',
+  'Котлетка',
+  'Кисло-сладкий соус',
+  'Помидорка',
+  'Салат',
+  'Кунжут',
+  'Сыр Чеддер',
+  'Майонез'
+];
 
-  const monteCristoBurgerIngredients = [
-    'Булка',
-    'Котлетка',
-    'Бекон',
-    'Яицо',
-    'Кисло-сладкий соус',
-    'Острый перец',
-    'Капуста',
-    'Лук',
-    'Сыр Грюйер',
-    'Майонез',
-    'Грибы'
-  ];
+const monteCristoBurgerIngredients = [
+  'Булка',
+  'Котлетка',
+  'Бекон',
+  'Яицо',
+  'Кисло-сладкий соус',
+  'Острый перец',
+  'Капуста',
+  'Лук',
+  'Сыр Грюйер',
+  'Майонез',
+  'Грибы'
+];
 
-  const shakeShackBurgerIngredients = [
-    'Булка',
-    'Огурчик',
-    'Котлетка',
-    'Соус карри',
-    'Помидорка',
-    'Острый перец',
-    'Капуста',
-    'Кунжут',
-    'Сыр Виолла',
-    'Майонез'
-  ];
+const shakeShackBurgerIngredients = [
+  'Булка',
+  'Огурчик',
+  'Котлетка',
+  'Соус карри',
+  'Помидорка',
+  'Острый перец',
+  'Капуста',
+  'Кунжут',
+  'Сыр Виолла',
+  'Майонез'
+];
 
-  const fishBurgerIngredients = [
-    'Булка',
-    'Рыбная котлета',
-    'Соус карри',
-    'Помидорка',
-    'Лимон',
-    'Капуста',
-    'Сыр Гауда',
-    'Майонез',
-    'Кетчуп'
-  ];
+const fishBurgerIngredients = [
+  'Булка',
+  'Рыбная котлета',
+  'Соус карри',
+  'Помидорка',
+  'Лимон',
+  'Капуста',
+  'Сыр Гауда',
+  'Майонез',
+  'Кетчуп'
+];
 
-  var OurMenu = [];
-  var OurOrders = [];
-  var menu = document.querySelector('#menu');
-  var ul = document.querySelector('#list');
+let OurMenu = [];
+let OurOrders = [];
+let menu = document.querySelector('#menu');
+let ul = document.querySelector('#list');
+let selectBurger = document.querySelector('#selectBurger');
+let buttonOrderOK = document.querySelector('.orderButton');
+let selectExcept = document.querySelector('#selectExcept');
+let selectAdd = document.querySelector('#selectAdd');
+let bubble = document.querySelector('.bubble');
+let speechBubblee = document.querySelector('.speech-bubble');
+let globalId = 1001;
+let globalOrder;
 
-function BurgerToMenu(name, ingredients, cookingTime) {
-    var text;
-    function Burger(){
-        this.name = name;
-        this.composition = this.showComposition();
-        this.ingredients;
-        this.cookingTime = cookingTime;
+function Burger(name, ingredients, cookingTime) {
+  this.name = name;
+  this.htmlName = '<span class="burgerName">' + this.name + '</span>' + ' <br> состав: ';
+  this.ingredients = ingredients;
+  this.cookingTime = cookingTime;
+
+  console.log(OurMenu);
+}
+Burger.prototype.showComposition = function(){
+    let {ingredients, name} = this;
+    let compositionLength = ingredients.length;
+    console.log(compositionLength);
+    if( compositionLength !== 0){
+      ingredients.map( function( item ){
+          console.log( 'Состав бургера', name, item );
+      });
     }
-
-
-    Burger.prototype.showComposition = function(){
-        let {composition} = this;
-        composition = ingredients;
-        let compositionLength = composition.length;
-        text = '<span class="burgerName">' + name + '</span>' + ' <br> состав: ';
-      
-        if( compositionLength !== 0) {
-            composition.map( function( item, index, arr ) {
-
-                if(index !== (compositionLength - 1) ) {
-                  text += item + ', ';
-                }
-                else {
-                  text += item + ';';
-                }
-
-            });
-        }
-        return text;
-  };
-
-  OurMenu.push(new Burger());
-  function addMenuToPage() {
-      list.insertAdjacentHTML('beforeEnd', '<li class="li">'+ text + '</li>');
-  }
-  addMenuToPage();
-  // console.log(OurMenu);
 }
-var cheesburger = new BurgerToMenu('Cheesburger', cheesburgerIngredients, 20);
-var monteCristoBurger = new BurgerToMenu('Monte Cristo', monteCristoBurgerIngredients, 15);
-var shakeShackBurger = new BurgerToMenu('Shake Shack', shakeShackBurgerIngredients, 10);
-var fishBurger = new BurgerToMenu('Fish Burger', fishBurgerIngredients, 25);
 
-var counter = 1;
-
-function getRandomInt(min, max) 
-{
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-function returnRandomBurger(arr) {
-    var randomIndex = getRandomInt(0, (arr.length - 1));
-    var randomBurger = arr[randomIndex];
-    return randomBurger;
-}
-function returnBurgersNameArray(arr) {
-    var burgerArray = arr.map(item => {
-        return item.name;
-    });
-    return burgerArray;
-}
-// function returnBurgersIngridientsArray(arr) {
-//     var burgerIngrArray;// код незавершений!!!
-// }
-var burgersArray = returnBurgersNameArray(OurMenu);
-
-(function toAddOrderToPage() {
-    var selectBurger = document.querySelector('#selectBurger');
-    for (var i = 0; i < burgersArray.length; i++) {
-        selectBurger.insertAdjacentHTML('beforeEnd', '<option>' + burgersArray[i] + '</option>');
-    }
-    var selectExcept = document.querySelector('#selectExcept');
-})();
-
-function Order(name, condition, value){
+function Order(name, condition, value) {
   this.name = name;
   this.condition = condition;
   this.value = value;
 
-  return this.returnOrderMessage();
+  this.returnOrderMessage();
 }
 Order.prototype.returnOrderMessage = function() {
-  var id = parseInt(1e10*Math.random());
-  var orderNumber = counter++;
-  var orderBurger,
-      cookingTime, 
-      orderException,
-      orderAvailability,
-      orderMessage,
-      valueText = '',
-      thisValue = this.value;
+  this.id = globalId++;
+  this.orderNumber = this.id;
+  this.orderBurger = this.name;
+  let orderMessage;
 
-  function makeTextValue() {
-      if(typeof thisValue === 'string') {
-           valueText = thisValue;
-      }
-      else if(Array.isArray(thisValue)) {
-           valueText = thisValue.join(', ');
-      }
-      else {
-        console.log('error');
-      }
-      return valueText;
-      console.log(valueText);
+  for (let i = 0; i < OurMenu.length; i++) {
+    if(OurMenu[i].name === this.name) {
+      this.cookingTime = OurMenu[i].cookingTime;
+      break;
+    }
   }
 
-  for(var i = 0; i < OurMenu.length; i++) {
-      if(OurMenu[i].name === this.name) {
-          orderBurger = OurMenu[i];
-          cookingTime = orderBurger.cookingTime;
+  switch (this.condition) {
+    case 'except':
+    {
+      this.orderException = this.value.join(',');
+      orderMessage = `Заказ ${this.orderNumber}: Бургер ${this.orderBurger}, except ${this.orderException}, будет готов через ${this.cookingTime} минут.`;
+      break;
+    }
+    case 'has':
+    {
+      this.orderAvailability = this.value.join(',');
+      if(this.name === 'My Burger') {
+        orderMessage = `Заказ ${this.orderNumber}: Бургер ${this.orderBurger}, what has ${this.orderAvailability}, будет готов через 20 минут.`;
+      } else {
+        orderMessage = `Заказ ${this.orderNumber}: Бургер ${this.orderBurger}, what has ${this.orderAvailability}, будет готов через ${this.cookingTime} минут.`;
       }
+      break;
+    }
+    case 'both':
+    {
+      this.orderException = this.value[0].join(',');
+      this.orderAvailability = this.value[1].join(',');
+      orderMessage = `Заказ ${this.orderNumber}: Бургер ${this.orderBurger} whithout ${this.orderException}, what has ${this.orderAvailability}, будет готов через ${this.cookingTime} минут.`;
+      break;
+    }
+    default:
+    {
+      //standart Burger from menu
+      orderMessage = `Заказ ${this.orderNumber}: Бургер ${this.orderBurger}, будет готов через ${this.cookingTime} минут.`;
+    }
   }
 
-  if(orderBurger && this.name && !this.condition && !thisValue) {
-      orderMessage = `Заказ ${orderNumber}: Бургер ${this.name}, будет готов через ${cookingTime} минут.`;
-  } 
-  else if(orderBurger && this.name && this.condition === 'except' && thisValue) {
-      orderException = thisValue;
-      makeTextValue();
-      orderMessage = `Заказ ${orderNumber}: Бургер ${this.name}, except ${valueText}, будет готов через ${cookingTime} минут.`;
+  bubble.classList.toggle('bubble_block', true);
+  speechBubblee.innerText = orderMessage;
+  console.log(orderMessage);
+}
+
+function initBugerMenu() {
+  OurMenu.forEach((item) => {
+    let text = item.htmlName;
+    if (item.ingredients.length > 0) {
+      item.ingredients.forEach((element, i, arr) => {
+        if (i === arr.length - 1) {
+          text += element + ';';
+        } else {
+          text += element + ',';
+        }
+      });
+    }
+    ul.insertAdjacentHTML('beforeEnd', '<li class="li">'+ text + '</li>');
+    selectBurger.insertAdjacentHTML('beforeEnd', '<option>' + item.name + '</option>');
+  });
+}
+
+function initOrderMenu() {
+  selectBurger.onchange = onSelectBurger;
+  buttonOrderOK.addEventListener('click', onOkClick);
+}
+
+function onSelectBurger(e) {
+  let ingredientsInChosenBurger = [];
+  let name = selectBurger.value;
+
+  initExceptOptions(name);
+  initAddOptions(name);
+}
+
+function initExceptOptions(burgerName) {
+  cleanAllChildNodes(selectExcept);
+
+  for(let i=0; i < OurMenu.length; i++) {
+    if(OurMenu[i].name === burgerName) {
+      ingredientsInChosenBurger = OurMenu[i].ingredients;
+      ingredientsInChosenBurger.forEach((ingredient) => {
+        let checkBoxDiv = `<div id="div_${ingredient}">
+        <input type="checkbox" id="${ingredient}" value="${ingredient}">
+        <label for="${ingredient}">${ingredient}</label>
+        </div>`;
+        selectExcept.insertAdjacentHTML('beforeEnd', checkBoxDiv);
+      });
+      break;
+    }
   }
-  else if(orderBurger && this.name && this.condition === 'has' && thisValue) {
-      orderAvailability = thisValue;
-      makeTextValue();
-      console.log(valueText);
-      orderMessage = `Заказ ${orderNumber}: Бургер ${this.name}, what has ${orderAvailability}, будет готов через ${cookingTime} минут.`;
+}
+
+function initAddOptions(burgerName) {
+  cleanAllChildNodes(selectAdd);
+
+  let differentIngredients = [];
+
+
+  for(let i=0; i < Ingredients.length; i++) {
+    if(ingredientsInChosenBurger.indexOf(Ingredients[i]) < 0) {
+      differentIngredients.push(Ingredients[i]);
+    }
   }
-  else if (!orderBurger) {
-      var randomBurgerArr = returnRandomBurger(OurMenu);
-      var randomBurger = randomBurgerArr.name;
-      var randomBurgerTime = randomBurgerArr.cookingTime;
-      var nonFoundMessage = `Burger with name "${this.name}" is absent. Maybe you want ${randomBurger}?`;
-      var maybeAnotherMessage;
-      var confirmOrder = confirm(nonFoundMessage);
-      if(confirmOrder) {
-          orderMessage = `Заказ ${orderNumber}: Бургер ${randomBurger}, будет готов через ${randomBurgerTime} минут.`;
-      }
-      else {
-          (function maybeAnotherMessageFunction() {
-              randomBurgerArr = returnRandomBurger(OurMenu);
-              randomBurger = randomBurgerArr.name;
-              randomBurgerTime = randomBurgerArr.cookingTime;
-              maybeAnotherMessage = confirm(`Maybe you want ${randomBurger}?`);
-              if(maybeAnotherMessage) {
-                  orderMessage = `Заказ ${orderNumber}: Бургер ${randomBurger}, будет готов через ${randomBurgerTime} минут.`;
-              }
-              else {
-                maybeAnotherMessageFunction();
-              }
-          })();
-      }
-  }        
-  console.log(orderMessage); 
-} 
 
-var order1 = new Order('Cheesburger');
-var order2 = new Order('Monte Cristo', 'except', 'onion');
-var order3 = new Order('Cheesburger', 'has', 'olives'); 
-var order4 = new Order('Cheesburger', 'except', ['onion', 'olives']); 
-var order5 = new Order('Cheesburger', 'has', ['fish', 'olives']); 
-var order6 = new Order('Burger', 'has', ['fish', 'olives']); 
+  differentIngredients.forEach((ingredient) => {
+    let checkBoxDiv = `<div id="div_${ingredient}">
+    <input type="checkbox" id="${ingredient}" value="${ingredient}">
+    <label for="${ingredient}">${ingredient}</label>
+    </div>`;
+    selectAdd.insertAdjacentHTML('beforeEnd', checkBoxDiv);
+  });
+}
+
+function cleanAllChildNodes(select) {
+  let nodes = select.childNodes;
+  let nodesLength = nodes.length;
+  while(nodes.length > 0) {
+    nodes[nodesLength-1].remove();
+    nodesLength--;
+  }
+}
+
+function calculateCondition(exceptOptions, addOptions) {
+  let except = exceptOptions.length > 0;
+  let add = addOptions.length > 0;
+  let both = except && add;
+
+  if(both) {
+    return 'both';
+  } else if(except) {
+    return 'except';
+  } else if(add) {
+    return 'has';
+  }
+}
+
+function calculatOptions(exceptOptions, addOptions) {
+    let except = exceptOptions.length > 0;
+    let add = addOptions.length > 0;
+    let both = except && add;
+
+    if(both) {
+      return [exceptOptions, addOptions];
+    } else if(except) {
+      return exceptOptions;
+    } else if(add) {
+      return addOptions;
+    }
+}
+
+function setDefaultState() {
+  //todo
+
+  // selectBurger.value = OurMenu[1].name;
+  // selectBurger.dispatchEvent(new Event('change'));
+}
 
 
+function onOkClick(event) {
+  let exceptOptions = [];
+  selectExcept.childNodes.forEach((item) => {
+    if(item.children[0].checked) {
+      exceptOptions.push(item.children[0].value);
+    }
+  });
+
+  let addOptions = [];
+  selectAdd.childNodes.forEach((item) => {
+    if(item.children[0].checked) {
+      addOptions.push(item.children[0].value);
+    }
+  });
+  let options;
+
+  // let MuBurgerIngridients =
+
+  let condition = calculateCondition(exceptOptions, addOptions);
+  let optionsIng = calculatOptions(exceptOptions, addOptions);
+  globalOrder = new Order(selectBurger.value, condition, optionsIng);
+
+
+  setDefaultState();
+}
+
+
+
+OurMenu.push(new Burger('Cheesburger', cheesburgerIngredients, 20));
+OurMenu.push(new Burger('Monte Cristo', monteCristoBurgerIngredients, 15));
+OurMenu.push(new Burger('Shake Shack', shakeShackBurgerIngredients, 10));
+OurMenu.push(new Burger('Fish Burger', fishBurgerIngredients, 25));
+OurMenu.push(new Burger('My Burger', ['Вы выбираете начинку сами'], 35));
+
+initOrderMenu();
+initBugerMenu();
+setDefaultState();
